c108c9746c28c71013df458918de5fb5222b54b5
ydah <t.yudai92@gmail.com>
2026-02-06 21:48:38 +0900

    Initial commit

---
+++ spec/rspec/rewind_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Rspec::Rewind do
+  it "has a version number" do
+    expect(Rspec::Rewind::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
